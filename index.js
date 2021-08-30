82e1812dacaeea9374f4c3d7947dd81e3adb643a
Candice <candice.vernacchio@gmail.com>
2021-08-29 18:50:42 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove(main);
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = 'is the champion';
